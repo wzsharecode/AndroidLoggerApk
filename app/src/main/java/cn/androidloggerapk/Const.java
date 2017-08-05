6d3574a232564888f90de534f98c9d2a2d28868c
computer <wangdeshun_com@163.com>
2017-08-05 17:48:01 +0800

    Initial commit

---
+++ app/src/main/java/cn/androidloggerapk/Const.java
@@ -0,0 +1,6 @@
+package cn.androidloggerapk;
+
+public class Const {
+    public static final int SDCARD_TYPE = 0; // 当前的日志记录类型为存储在SD卡下面
+    public static final int MEMORY_TYPE = 1; // 当前的日志记录类型为存储在内存中
+}
